a00e5ed040ea7c4bf61e497d5eaf1c16175970a7
adrien <adrien.schmitz@gmail.com>
2021-11-13 09:26:44 -0300

    Added views to create/edit diario day inside the site

--- meusite/diario/urls.py
+++ meusite/diario/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.day_list, name='day_list'),
     path("update_server/", views.update, name="update"),
     path('day/<int:pk>/', views.day_detail, name='day_detail'),
+    path('day/new/', views.day_new, name='day_new'),
+    path('day/<int:pk>/edit', views.day_edit, name='day_edit'),
 ]
--- meusite/diario/views.py
+++ meusite/diario/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 import git
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Day
 from django.utils import timezone
+from .forms import DayForm
 
 
 def day_list(request):
@@ -20,7 +21,37 @@ def day_detail(request, pk):
     return render(request, "diario/day_detail.html", {'day': day})
 
 
+def day_new(request):
+    if request.method == "POST":
+        form = DayForm(request.POST)
+        if form.is_valid():
+            day = form.save(commit=False)
+            day.author = request.user
+            day.published_date = timezone.now()
+            day.save()
+            return redirect('day_detail', pk=day.pk)
+    else:
+        form = DayForm()
+    return render(request, 'diario/day_edit.html', {'form': form})
+
+
+def day_edit(request, pk):
+    day = get_object_or_404(Day, pk=pk)
+    if request.method == "POST":
+        form = DayForm(request.POST, instance=day)
+        if form.is_valid():
+            day = form.save(commit=False)
+            day.author = request.user
+            day.published_date = timezone.now()
+            day.save()
+            return redirect('day_detail', pk=day.pk)
+    else:
+        form = DayForm(instance=day)
+    return render(request, 'diario/day_edit.html', {'form': form})
+
 # Git Webhook
+
+
 @csrf_exempt
 def update(request):
     if request.method == "POST":
